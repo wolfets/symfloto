be58e758e07d6be6ccd24c5c123a4e9cea68437d
std <wolfets@free.fr>
2015-06-20 08:15:02 +0200

    démarrage

---
+++ src/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/")
+     * @Route("/app/example", name="homepage")
+     * @Template()
+     */
+    public function indexAction()
+    {
+        return $this->render('default/index.html.twig', array('hello'=>'hellooooo'));
+    }
+}
